f2ba1bdd457d9e0710980f642c5339321393a44f
Julian Stecklina <js@alien8.de>
2022-03-01 13:36:48 +0100

    Build packet forwarder app

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = rec {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -11,8 +13,23 @@
     flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
   };
 
-  outputs = { self, nixpkgs, flake-compat, flake-compat-ci }: {
+  outputs = { self, nixpkgs, utils, naersk, flake-compat, flake-compat-ci }:
+    (utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.pkt_fwd = naersk-lib.buildPackage {
+        pname = "pkt_fwd";
+        root = ./pkt_fwd;
+      };
+      defaultPackage = packages.pkt_fwd;
 
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo ];
+      };
+    })) // {
     nixosConfigurations = {
       diode = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -22,6 +39,8 @@
         ];
       };
     };
+
+  } // {
 
     # For Hercules CI, which doesn't natively support flakes (yet).
     ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
